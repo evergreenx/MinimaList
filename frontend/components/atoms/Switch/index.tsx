62e8b8b131be6aff69a262e75b13b3e3e4cc2a70
ido evergeen <dicksonevegreenido@gmail.com>
2023-05-29 23:42:12 +0100

    fix today switch error

--- frontend/components/atoms/Switch/index.tsx
+++ frontend/components/atoms/Switch/index.tsx
@@ -17,7 +17,7 @@ export const SwitchToday = () => (
       <Switch.Root
         className="w-[51px] h-[31px] bg-black rounded-full relative  data-[state=checked]:bg-[#34C759] outline-none cursor-default"
         id="airplane-mode"
-        style={{ "-webkit-tap-highlight-color": "rgba(0, 0, 0, 0)" }}
+        // style={{ "-webkit-tap-highlight-color": "rgba(0, 0, 0, 0)" }}
       >
         <Switch.Thumb className="block w-[27px] h-[27px] bg-white rounded-full shadow-[0_2px_2px] shadow-blackA7 transition-transform duration-100 translate-x-0.5 will-change-transform data-[state=checked]:translate-x-[19px]" />
       </Switch.Root>

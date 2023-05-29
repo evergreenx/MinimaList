f314c4ac473e56edfd41a406b00f11df9268571b
ido evergeen <dicksonevegreenido@gmail.com>
2023-05-29 23:49:51 +0100

    fix screen height

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${poppins.className}  bg-[#F7F8FA] h-screen`}>
+      <body className={`${poppins.className}  bg-[#F7F8FA] `}>
         <Provider>{children}</Provider>
       </body>
     </html>
